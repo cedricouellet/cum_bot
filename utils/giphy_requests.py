77779f4c425bd176824e82754e19f9204e4e22a8
Cedric-Antoine Ouellet <1925921@cstjean.qc.ca>
2021-11-27 01:39:37 -0500

    testing errors

--- utils/giphy_requests.py
+++ utils/giphy_requests.py
@@ -16,6 +16,8 @@ def __make_giphy_request(search: str = None) -> str:
     :param search: The search query
     :return: A gif according to the search, or a random gif
     """
+    print(GIPHY_API_KEY)
+    return "testing"
     query_url = f'{GIPHY_API_URL}/search?q={search}&api_key={GIPHY_API_KEY}&limit=1'
     if search is None:
         query_url = f'{GIPHY_API_URL}/random?api_key={GIPHY_API_KEY}&limit=1'
