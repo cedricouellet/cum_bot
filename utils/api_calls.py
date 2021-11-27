9a5decbe6f2e04811fea2d1c4505988cbe19cc10
Cedric-Antoine Ouellet <1925921@cstjean.qc.ca>
2021-11-26 21:18:54 -0500

    gif in progress

--- utils/api_calls.py
+++ utils/api_calls.py
@@ -34,8 +34,7 @@ def __make_giphy_request(search: str, giphy_api_key: str) -> str:
         query_url += f'search?q={search}&api_key={giphy_api_key}&limit=1'
 
     gif = requests.get(query_url).json()
-    url = gif["data"][0]
-    print(url)
+    url = gif["data"][0]["url"]
 
     return url
 
